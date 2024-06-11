a89c8f20662c230a1e8ede7ef5a2d2a3e0339959
Sergei Lukashin <lsv1387@gmail.com>
2024-06-11 16:12:48 +0300

    Var_names_1.0

---
+++ Lesson_1/VariableNamesTheme.java
@@ -0,0 +1,163 @@
+public class VariableNamesTheme {
+    public static void main(String[] args) {
+        System.out.println("Разные переменные");
+        //цифра
+        int digit = 10;
+        System.out.println("цифра = " + digit);
+
+        //сумма чисел
+        int sumOfNums = 160;
+        System.out.println("cумма чисел = " + sumOfNums);
+
+        //произведение цифр
+        int multiplicationOfdigits = 16200;
+        System.out.println("произведение цифр = " + multiplicationOfdigits);
+
+        //максимальное число
+        int maxNumber = 999999;
+        System.out.println("максимальное число = " + maxNumber);
+
+        //количество десятков
+        int countOfTens = 10;
+        System.out.println("количество десятков = " + countOfTens);
+
+        //вес собаки
+        float dogWeigh = 8.6f;
+        System.out.println("вес собаки = " + dogWeigh + " кг");
+
+        //исходное число
+        int initialNumber = 100;
+        System.out.println("исходное число = " + initialNumber);
+
+        //процент по вкладу
+        int depositRate = 13;
+        System.out.println("процент по вкладу = " + depositRate + "%");
+
+        //переменная хранит символ &
+        char variableStores = '&';
+        System.out.println("переменная хранит символ " + variableStores);
+
+        //код ошибки
+        int errorCode = 404;
+        System.out.println("код ошибки = " + errorCode);
+
+        //тип сообщения
+        int typeOfMessage = 1;
+        System.out.println("тип сообщения = " + typeOfMessage);
+
+        //число нулей
+        int numberOfZeros = 3;
+        System.out.println("число нулей = " + numberOfZeros);
+
+        //уникальное число
+        int uniqueNumber = 333;
+        System.out.println("уникальное число = " + uniqueNumber);
+
+        //случайное число
+        int randomNumber = 123;
+        System.out.println("случайное число = " + randomNumber);
+
+        //математическое выражение
+        double mathExpression = (double) (maxNumber - randomNumber) / depositRate;
+        System.out.println("математическое выражение = " + mathExpression);
+
+        //счет в игре
+        int gameScore = 0;
+        System.out.println("счет в игре = " + gameScore);
+
+        //максимальная длина
+        int maxLength = 650;
+        System.out.println("максимальная длина = " + maxLength + "px");
+
+        //пункт меню
+        int menuItem = 1;
+        System.out.println("пункт меню: " + menuItem);
+
+        //стоимость кофе
+        float coffeePrice = 2.5f;
+        System.out.println("стоимость кофе: " + coffeePrice + "$");
+
+        //дата начала
+        String startDate = "01.01.1970";
+        System.out.println("дата начала = " + startDate);
+
+        //окончание диапазона
+        int endOfrange = 5;
+        System.out.println("окончание диапазона = " + endOfrange);
+
+        //полное имя работника месяца
+        String emploeeOfTheMonthFullName = "Лукашин Сергей Владимирович";
+        System.out.println("полное имя работника месяца: " + emploeeOfTheMonthFullName);
+
+        //заголовок электронной книги
+        String eBookTitle = "Lord of the ring";
+        System.out.println("заголовок электронной книги " + eBookTitle);
+
+        //размер
+        int size = 1024;
+        System.out.println("размер = " + size + "px");
+
+        //вместимость
+        int capacity = 25;
+        System.out.println("вместимость = " + capacity);
+
+        //счетчик
+        int counter = 0;
+        System.out.println("счетчик = " + counter);
+
+        //путь до файла
+        String filePath = "D:/games/quake.exe";
+        System.out.println("путь до файла " + "\""+filePath+"\"");
+
+        //количество чисел в каждой строке
+        int countOfNumbersEachLine = 350;
+        System.out.println("количество чисел в каждой строке = " + countOfNumbersEachLine);
+
+        System.out.println("\nboolean-переменные");
+        //сотни равны?
+        boolean isHundredsEqual = false;
+        System.out.println("сотни равны? " + isHundredsEqual);
+
+        //компьютер включен?
+        boolean isComputerOn = true;
+        System.out.println("компьютер включен? " + isComputerOn);
+
+        //есть равные цифры?
+        boolean hasEqualNumbers = true;
+        System.out.println("есть равные цифры? " + hasEqualNumbers);
+
+        //создано?
+        boolean isCreated = false;
+        System.out.println("создано? " + isCreated);
+
+        //пустое?
+        boolean isEmpty = true;
+        System.out.println("пустое? " + isEmpty);
+
+        //активное?
+        boolean isActive = true;
+        System.out.println("активное? " + isActive);
+
+        //новое?
+        boolean isNew = false;
+        System.out.println("новое? " + isNew);
+
+        //электронная почта действительная?
+        boolean isEmailValid = false;
+        System.out.println("электронная почта действительная? " + isEmailValid);
+
+        //имеются уникальные строки?
+        boolean hasUniqueStrings = true;
+        System.out.println("имеются уникальные строки? " + hasUniqueStrings);
+
+        System.out.println("\nАббревиатуры ");
+        //старый universally unique identifier
+        //производитель оперативной памяти
+        //емкость жесткого диска
+        //протокол передачи гипертекста
+        //сокращенный uniform resource locator
+        //новый идентификатор клиента
+        //american standard code for information interchange
+
+    }
+}
